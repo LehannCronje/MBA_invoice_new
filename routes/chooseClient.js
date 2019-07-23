b927f1785ed81bdb58047b51a0e1cbcd39153439
stefanatg2 <stefan@g2design.co.za>
2019-07-23 12:42:10 +0200

    minor changes

--- routes/chooseClient.js
+++ routes/chooseClient.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../utils/db_config');
 
-router.post('/', (req,res, next) => {
-    res.render('chooseClient');
+router.post('/', getClientSups,(req,res, next) => {
+    res.render('chooseClient' , {client: req.client, sup: req.sup});
 });
+
+function getClientSups(req,res,next){
+    var sql = 'select Cust_Name from customer where CustomerID = 1001';
+    db.query(sql, (error,results,fields) => {
+        if(error){
+            return console.error(error.message);
+        }
+        req.client = results[0].Cust_Name;
+    });
+    var sql = 'select SupplierName from supplier where SupplierID = 1001';
+    db.query(sql, (error,results,fields) => {
+        if(error){
+            return console.error(error.message);
+        }
+        req.sup = results[0].SupplierName;
+        return next();
+    })
+}
 
 module.exports = router;
