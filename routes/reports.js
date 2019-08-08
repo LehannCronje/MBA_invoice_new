ea22d76c3f0abbc67c443b22a227a58f638f499c
LehannG2 <37569304+LehannG2@users.noreply.github.com>
2019-08-08 09:36:10 +0200

    changes to report page

--- routes/reports.js
+++ routes/reports.js
@@ -4,21 +4,15 @@ var db = require('../utils/db_config');
 var database = new db();
 
 
-const fakeDatabase = {
-    'philip' : {job: 'professor', pet: 'cat.jpg'},
-    'John' : {job: 'student', pet: 'dog.jpg'},
-    'Carol' : {job: 'engineer', pet: 'bear.jpg'},
-  }
-  
-  router.get('/reportsData', (req,res) => {
-      var sql = "CALL `DB_MBA2`.`usp_MonthlyTransactionFee`('2019-07-02','2019-07-11');"
+  router.post('/reportsData', (req,res) => {
+      var sql = "CALL `DB_MBA2`.`usp_MonthlyTransactionFee`('"+req.body.date1+"','"+req.body.date2+"');"
     database.query(sql).then(rows =>{
         res.send(rows);
     })
     
   });
-  router.get('/reportsData2', (req,res) => {
-    var sql = "CALL `DB_MBA2`.`usp_InvoiceSummary`(82481);"
+  router.post('/reportsData2', (req,res) => {
+    var sql = "CALL `DB_MBA2`.`usp_InvoiceSummary`('"+req.body.data+"');"
     database.query(sql).then(rows =>{
       res.send(rows);
     })
